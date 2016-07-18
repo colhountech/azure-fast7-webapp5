bca1a904cacf9fd8e10461a79cdbab09cbf57f42
Micheal Colhoun <micheal@cts-ni.com>
2016-07-18 23:45:20 +0100

    added missing tests

---
+++ test/webapp5.tests/Class1.cs
@@ -0,0 +1,21 @@
+using Xunit;
+
+namespace webapp5.tests
+{
+    // see example explanation on xUnit.net website:
+    // https://xunit.github.io/docs/getting-started-dotnet-core.html
+    public class Class1
+    {
+        [Fact]
+        public void PassingTest()
+        {
+            Assert.Equal(4, Add(2, 2));
+        }
+
+
+        int Add(int x, int y)
+        {
+            return x + y;
+        }
+    }
+}
